09711b98d4ff0361f9c023e8cc57d7bb207101a3
diana <diana.niebla@coppel.com>
2025-03-11 11:17:05 -0700

    Update index.js

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -4,8 +4,13 @@
 
 //getImagenPromesa().then(console.log);
 
+
+
 const getImagen = async() => {
-    
+    const apiKey = 'ZsHigdk5nw6xQRYWC2ige7ShTiR3sn4i';
+    const resp = await fetch (`http://api.giphy.copm/v1/gifs/random?api_key=${ apiKey}`);
+    const data = await resp.json();
+
+    console.log(data);
 }
 
-getImagen().then(console.log)
